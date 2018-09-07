781b2abbbb4af1cc99dd34b9e3207e43ba78ff56
jwp3 <jwp3@gatech.edu>
2018-09-06 23:33:14 -0400

    added semi-asynchronous Gauss-Seidel smoother

--- src/SMEM_Smooth.cpp
+++ src/SMEM_Smooth.cpp
@@ -34,6 +34,36 @@ void SMEM_Sync_Parfor_Jacobi(AllData *all_data,
    }
 }
 
+void SMEM_SemiAsync_Parfor_GaussSeidel(AllData *all_data,
+                                      hypre_CSRMatrix *A,
+                                      HYPRE_Real *f,
+                                      HYPRE_Real *u,
+                                      int num_sweeps)
+{
+   HYPRE_Int ii;
+   HYPRE_Real res;
+
+   HYPRE_Int n = hypre_CSRMatrixNumRows(A);
+
+   double smooth_weight = all_data->input.smooth_weight;
+
+   for (int k = 0; k < num_sweeps; k++){
+      #pragma omp for
+      for (int i = 0; i < n; i++){
+         if (A->data[A->i[i]] != 0.0)
+         {
+            res = f[i];
+            for (int jj = A->i[i]; jj < A->i[i+1]; jj++)
+            {
+               ii = A->j[jj];
+               res -= A->data[jj] * u[ii];
+            }
+            u[i] += smooth_weight * res / A->data[A->i[i]];
+         }
+      }
+   }
+}
+
 void SMEM_Sync_Parfor_HybridJacobiGaussSeidel(AllData *all_data,
                                               hypre_CSRMatrix *A,
                                               HYPRE_Real *f,
@@ -106,6 +136,37 @@ void SMEM_Sync_Jacobi(AllData *all_data,
       SMEM_LevelBarrier(all_data, thread_level);
    }
 }
+
+void SMEM_SemiAsync_GaussSeidel(AllData *all_data,
+                                hypre_CSRMatrix *A,
+                                HYPRE_Real *f,
+                                HYPRE_Real *u,
+                                int num_sweeps,
+                                int thread_level,
+                                int ns, int ne)
+{
+   HYPRE_Int ii;
+   HYPRE_Real res;
+
+   HYPRE_Int n = hypre_CSRMatrixNumRows(A);
+
+   for (int k = 0; k < num_sweeps; k++){
+      for (int i = ns; i < ne; i++){
+         if (A->data[A->i[i]] != 0.0)
+         {
+            res = f[i];
+            for (int jj = A->i[i]; jj < A->i[i+1]; jj++)
+            {
+               ii = A->j[jj];
+               res -= A->data[jj] * u[ii];
+            }
+            u[i] += res / A->data[A->i[i]];
+         }
+      }
+      SMEM_LevelBarrier(all_data, thread_level);
+   }
+}
+
 
 void SMEM_Sync_HybridJacobiGaussSeidel(AllData *all_data,
                                        hypre_CSRMatrix *A,
--- src/SMEM_Smooth.hpp
+++ src/SMEM_Smooth.hpp
@@ -10,6 +10,12 @@ void SMEM_Sync_Parfor_Jacobi(AllData *all_data,
                              HYPRE_Real *u_prev,
                              int num_sweeps);
 
+void SMEM_SemiAsync_Parfor_GaussSeidel(AllData *all_data,
+                                      hypre_CSRMatrix *A,
+                                      HYPRE_Real *f,
+                                      HYPRE_Real *u,
+                                      int num_sweeps);
+
 void SMEM_Sync_Parfor_HybridJacobiGaussSeidel(AllData *all_data,
                                               hypre_CSRMatrix *A,
                                               HYPRE_Real *f,
@@ -26,6 +32,14 @@ void SMEM_Sync_Jacobi(AllData *all_data,
                       int num_sweeps,
                       int thread_level,
                       int ns, int ne);
+
+void SMEM_SemiAsync_GaussSeidel(AllData *all_data,
+                                hypre_CSRMatrix *A,
+                                HYPRE_Real *f,
+                                HYPRE_Real *u,
+                                int num_sweeps,
+                                int thread_level,
+                                int ns, int ne);
 
 void SMEM_Sync_HybridJacobiGaussSeidel(AllData *all_data,
                                        hypre_CSRMatrix *A,
