ce94b5e95cdf306b6e53bf10206f83a0dd300b34
jwp3 <jwp3@gatech.edu>
2018-09-24 23:19:06 -0400

    minor changes

--- ResHist_Mfem.sh
+++ ResHist_Mfem.sh
@@ -7,8 +7,8 @@ async_type=${4}
 coarsen_type=${5}
 agg_nl=1
 ref_levels=4
-problem="mfem"
-num_smooth_sweeps=1
+problem="mfem_laplace"
+num_smooth_sweeps=2
 
 file_directory="data/ResHist_Mfem/"
 file_name="${file_directory}SOLVER-${solver}-SMOOTHER-${smoother}-SWEEPS-${num_smooth_sweeps}-PROBLEM-${problem}-NUMTHREADS-${num_threads}-ASYNCTYPE-${async_type}-COARSENTYPE-${coarsen_type}-AGGNL-${agg_nl}-REFLEVELS-${ref_levels}-"
@@ -20,4 +20,4 @@ num_cycles=50
 start_cycle=2
 incr_cycle=2
 
-KMP_AFFINITY=balanced,granularity=fine ./Main -format_output -num_runs ${num_runs} -num_cycles ${num_cycles} -start_cycle ${start_cycle} -incr_cycle ${incr_cycle} -mfem_ref_levels ${ref_levels} -num_threads ${num_threads} -solver ${solver} -smoother ${smoother} -problem ${problem} -async_type ${async_type} -agg_nl ${agg_nl} -smooth_weight .6 -converge_test_type all -num_smooth_sweeps ${num_smooth_sweeps} | tee -a ${file_name}
+KMP_AFFINITY=compact ./Main -format_output -no_reshist -num_runs ${num_runs} -num_cycles ${num_cycles} -start_cycle ${start_cycle} -incr_cycle ${incr_cycle} -mfem_ref_levels ${ref_levels} -num_threads ${num_threads} -solver ${solver} -smoother ${smoother} -problem ${problem} -async_type ${async_type} -agg_nl ${agg_nl} -smooth_weight .6 -converge_test_type all -num_smooth_sweeps ${num_smooth_sweeps} | tee -a ${file_name}
--- VaryProbSize.sh
+++ VaryProbSize.sh
@@ -7,18 +7,19 @@ num_threads=${4}
 async_type=${5}
 coarsen_type=${6}
 agg_nl=0
+num_smooth_sweeps=1
 
 file_directory="data/VaryProbSize/"
-file_name="${file_directory}SOLVER-${solver}-SMOOTHER-${smoother}-PROBLEM-${problem}-NUMTHREADS-${num_threads}-ASYNCTYPE-${async_type}-COARSENTYPE-${coarsen_type}-AGGNL-${agg_nl}-"
+file_name="${file_directory}SOLVER-${solver}-SMOOTHER-${smoother}-SWEEPS-${num_smooth_sweeps}-PROBLEM-${problem}-NUMTHREADS-${num_threads}-ASYNCTYPE-${async_type}-COARSENTYPE-${coarsen_type}-AGGNL-${agg_nl}-"
 mkdir -p ${file_directory}
 rm -f ${file_name}
 
 num_runs=20
-num_cycles=20
+num_cycles=50
 #n_list=(10 20 50 100 150)
-n_list=(100 200 500 1000 1500)
+n_list=(65 129 257 513 1025)
 
 for n in ${n_list[@]}
 do
-	KMP_AFFINITY=balanced,granularity=fine ./Main -format_output -num_runs ${num_runs} -n ${n} -num_cycles ${num_cycles} -num_threads ${num_threads} -solver ${solver} -problem ${problem} -async_type ${async_type} -converge_test_type all | tee -a ${file_name}
+	KMP_AFFINITY=compact ./Main -format_output -smoother ${smoother} -num_smooth_sweeps ${num_smooth_sweeps} -num_runs ${num_runs} -n ${n} -num_cycles ${num_cycles} -num_threads ${num_threads} -solver ${solver} -problem ${problem} -async_type ${async_type} -converge_test_type all | tee -a ${file_name}
 done
